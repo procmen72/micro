9ed67f4d237125df2333048dd646e9a0d6c2ae17
Alejandro Menchaca <alejandro.menchaca@axity.com>
2021-05-26 11:55:52 -0500

    Se agrega arquetipo y apigateway

---
+++ practica/ApiGateway/src/main/java/mx/com/axity/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package mx.com.axity.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
---
+++ practica/ApiGateway/src/test/java/mx/com/axity/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.axity.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
